12fda556f542cefa67c551b460d3511b2c4b2bb8
Adelaide Li <adelaide.li2009@gmail.com>
2026-06-09 18:18:24 -0400

    improved CSS and styling

--- static/script.js
+++ static/script.js
@@ -1,4 +1,17 @@
-// const form = document.getElementById("analysis-form");
-// form.addEventListener("submit", function() {
-//     document.getElementById("loading").style.display = "block";
-// });
+const images = [
+    "/static/assets/americanFlag.jpg",
+    "/static/assets/breakingNews.jpg",
+    "/static/assets/newspaper.jpg"
+];
+
+let current = 0;
+const header = document.getElementById("header");
+function changeBackground() {
+    header.style.backgroundImage =
+        `url('${images[current]}')`;
+    current = (current + 1) % images.length;
+}
+// Set initial image
+changeBackground();
+// Change every five seconds
+setInterval(changeBackground, 5000);
